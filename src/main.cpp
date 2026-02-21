423a561449541b5a2f584a517eab1e22268cca3d
Vedabahu <79869936+Vedabahu@users.noreply.github.com>
2026-02-22 01:12:19 +0530

    Made templates and compression work

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <crow.h>
+#include <filesystem>
+#include <string>
 
-int main() {
-  crow::SimpleApp app;
+int main()
+{
+    std::filesystem::path source_file = __FILE__;
+    std::filesystem::path source_dir = source_file.parent_path();
+    std::filesystem::path templates_path = source_dir / "templates";
 
-  CROW_ROUTE(app, "/")([]() { return "Crow is working."; });
+    crow::App app;
+    app.use_compression(crow::compression::algorithm::GZIP);
+    crow::mustache::set_global_base(templates_path.string());
 
-  app.port(18080).multithreaded().run();
+    CROW_ROUTE(app, "/")([]() {
+        crow::mustache::template_t page = crow::mustache::load("hello.html");
+        return page.render();
+    });
+
+    CROW_ROUTE(app, "/hello/<string>")([](std::string name) {
+        crow::mustache::context ctx;
+        ctx["name"] = name;
+        crow::mustache::template_t page = crow::mustache::load("hello_name.html");
+        return page.render(ctx);
+    });
+
+    app.port(18080).multithreaded().run();
 }
